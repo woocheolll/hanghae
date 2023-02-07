2e73dfe48d799eb740243766250a2d29b652a392
woocheolll <woocheol2868@gmail.com>
2023-02-07 16:05:42 +0900

    웹 get, post 문제

--- projects/homework/app.py
+++ projects/homework/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:as123123@cluster0.nnsglfi.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 app = Flask(__name__)
 
 
@@ -12,18 +14,22 @@ def home():
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
 
-
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+    db.fan.insert_one(doc)
+    return jsonify({'msg': '작성완료'})
 
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
+    comment_list = list(db.fan.find({}, {'_id': False}))
 
-
-    return jsonify({'msg': 'GET 연결 완료!'})
+    return jsonify({'comments':comment_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
